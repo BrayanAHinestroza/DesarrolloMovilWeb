dcc9b12facfddeaa430154a87738861b1a2a9b92
Santiago Marmolejo <santiagomc2k16@gmail.com>
2022-04-08 21:55:37 -0500

    Connect to sql

--- api/src/config.js
+++ api/src/config.js
@@ -1,13 +1,13 @@
 const config = {}
 
 //Define el puerto del servidor
-config.PORT = 5000;
+config.PORT = 3306;
 
 //Define informacion de DB
-config.DB_HOST = "localhost"
-config.DB_USER = "root"
-config.DB_PASS = ""
-config.DB_DATABASE = "bd_sgc"
+config.DB_HOST = "bqhd6oox3mqor4myj3fv-mysql.services.clever-cloud.com"
+config.DB_USER = "uu5lloejsgybvr9b"
+config.DB_PASS = "nMGLjqOCO8iQ173dnCit"
+config.DB_DATABASE = "bqhd6oox3mqor4myj3fv"
 
 config.SECRET = 'sinapsisUAO-APP';
 
